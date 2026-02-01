8c163a2a06f6c8a6793f52beeea3c1d6b9e9945a
Hanifa Islam <220178552@aston.ac.uk>
2026-02-01 22:33:48 +0000

    edit database .js

--- db.js
+++ db.js
@@ -10,9 +10,9 @@ const db = mysql.createConnection({
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err);
-    return;
+  } else {
+    console.log("Connected to MySQL database");
   }
-  console.log("Connected to MySQL database");
 });
 
 module.exports = db;
