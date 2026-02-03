3544db5270f11eb6c5d9c70e7c075db404534794
Hanifa Islam <220178552@aston.ac.uk>
2026-02-03 23:40:49 +0000

    added to server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const usersRoutes = require("./routes/users");
+
+app.use(express.json());
+app.use("/api/users", usersRoutes);
 
 app.get("/", (req, res) => {
   res.send("Roommate App Backend is running 🚀");
