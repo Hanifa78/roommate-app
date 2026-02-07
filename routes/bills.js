f07e2f73b4301edcde62336c98ebaa60f00bc5f4
Hanifa Islam <220178552@aston.ac.uk>
2026-02-07 21:06:12 +0000

    automatically split bills

--- routes/bills.js
+++ routes/bills.js
@@ -19,10 +19,41 @@ router.post("/create", (req, res) => {
         return res.status(500).json(err);
       }
 
-      res.status(201).json({
-        message: "Bill created",
-        billId: result.insertId
-      });
+     const billId = result.insertId;
+
+// Get all household members
+const membersSql = `
+  SELECT user_id
+  FROM household_members
+  WHERE household_id = ?
+`;
+
+db.query(membersSql, [householdId], (err, members) => {
+  if (err) return res.status(500).json(err);
+
+  const splitAmount = amount / members.length;
+
+  const shareSql = `
+    INSERT INTO bill_shares (bill_id, user_id, amount)
+    VALUES ?
+  `;
+
+  const values = members.map(m => [
+    billId,
+    m.user_id,
+    splitAmount
+  ]);
+
+  db.query(shareSql, [values], (err2) => {
+    if (err2) return res.status(500).json(err2);
+
+    res.status(201).json({
+      message: "Bill created and split",
+      billId
+    });
+  });
+});
+
     }
   );
 });
